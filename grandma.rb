ea5a9a8daec1280bd271617ab6bc04f61fc37577
Scott  Keplinger <skeplin17@gmail.com>
2020-05-04 23:33:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
- if phrase != uppercase
+ if phrase != upcase
    puts "HUH?! SPEAK UP, SONNY!"
- elsif phrase == uppercase
+ elsif phrase == upcase
    puts "NO, NOT SINCE 1938!"
  else phrase == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
